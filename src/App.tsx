24d7f9415edfc310005f5541f932258f50937aa8
kyou-nase <carldominique05@gmail.com>
2026-02-03 22:20:30 +0800

    Initialized File

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { World } from './pages/World';
+import { Background } from './components/Background';
+
+function App() {
+  return (
+    <Router>
+      <Background />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/world" element={<World />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
