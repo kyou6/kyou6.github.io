88f3a8c0560942af87b5c027e6a49513ca4b31ff
kyou-nase <carldominique05@gmail.com>
2026-02-04 01:38:01 +0800

    Added Statistics Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { World } from './pages/World';
+import { Statistics } from './pages/Statistics';
 import { Background } from './components/Background';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/world" element={<World />} />
+        <Route path="/statistics" element={<Statistics />} />
       </Routes>
     </Router>
   );
